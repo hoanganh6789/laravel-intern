84742ee9f30064b1f888c35b71b024b2e9bf92e6
anhnhph33991 <hoang838604@gmail.com>
2024-10-17 09:56:34 +0700

    feat: add product color managers

--- app/Http/Controllers/Admin/ProductColorController.php
+++ app/Http/Controllers/Admin/ProductColorController.php
@@ -2,24 +2,63 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helper\Alert;
+use App\Helper\Toastr;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductColorRequest;
+use App\Http\Requests\UpdateProductColorRequest;
+use App\Models\ProductColor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductColorController extends Controller
 {
     private const VIEW_PATH = "admin.product_color.";
     public function index()
     {
-        return view(self::VIEW_PATH . __FUNCTION__);
+        $productColors = ProductColor::query()->latest('id')->paginate(10);
+        return view(self::VIEW_PATH . __FUNCTION__, compact('productColors'));
     }
 
     public function create()
     {
         return view(self::VIEW_PATH . __FUNCTION__);
     }
-    public function store() {}
+    public function store(StoreProductColorRequest $request) {
+        try {
+            ProductColor::create($request->validated());
+            Toastr::success(null, 'Thêm màu thành công');
+            return redirect()->route('admin.product-colors.index');
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Toastr::error(null, 'Thêm màu không thành công');
+            return back();
+        }
+    }
     public function show() {}
-    public function edit() {}
-    public function update() {}
-    public function destroy() {}
+    public function edit(ProductColor $productColor) {
+        return view(self::VIEW_PATH . __FUNCTION__, compact('productColor'));
+    }
+    public function update(UpdateProductColorRequest $request, ProductColor $productColor) {
+        try {
+            $productColor->update($request->validated());
+            Toastr::success(null, 'Sửa màu thành công');
+            return redirect()->route('admin.product-colors.index');
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Alert::error('Lỗi khi sửa', "Luxchill Thông Báo");
+            return back();
+        }
+    }
+    public function destroy(ProductColor $productColor) {
+        try {
+            $productColor->delete();
+            Alert::success('Xóa thành công', "Luxchill Thông Báo");
+            return back();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Alert::error('Lỗi khi xóa', "Luxchill Thông Báo");
+            return back();
+        }
+    }
 }
--- database/migrations/2024_09_15_193423_create_product_colors_table.php
+++ database/migrations/2024_09_15_193423_create_product_colors_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_colors', function (Blueprint $table) {
             $table->id();
-
-            $table->string('name', 255);
-            
-
+            $table->string('name', 255)->unique();
             $table->timestamps();
         });
     }
