a6f58b29f02e8f77c47eaf8e7cd1ce20ee4a83f0
anhnhph33991 <hoang838604@gmail.com>
2024-12-15 03:02:46 +0700

    begin: 2w

--- app/Http/Controllers/Client/CheckOutController.php
+++ app/Http/Controllers/Client/CheckOutController.php
@@ -24,31 +24,69 @@ class CheckOutController extends Controller
         $this->cartServices = $cartService;
     }
 
+    // public function index()
+    // {
+    //     $userId = Auth::id();
+
+    //     $orderId = request()->orderId ?: null;
+    //     $resultCode = request()->resultCode ?: null;
+
+    //     $cart = [];
+    //     $total = 0;
+
+    //     if ($userId) {
+    //         [$cart, $total] = $this->cartServices->getCartWithTotal($userId);
+    //     } else {
+    //         if (session()->has('cart') && session()->get('total')) {
+    //             $cart = session('cart');
+    //             $total = session('total');
+    //         }
+    //     }
+
+    //     if (empty($cart)) {
+    //         return redirect()->route('shop.index');
+    //     }
+
+    //     if (request()->has('orderId') && request()->has('resultCode') && $resultCode == 0) {
+    //         return $this->checkOutServices->isMomo();
+    //     }
+
+    //     return view(self::PATH_VIEW . 'check-out', compact('cart', 'total'));
+    // }
+
+    // public function handle(StoreOrderClientRequest $request)
+    // {
+
+    //     // StoreOrderClientRequest
+    //     if ($request->payment == 'momo') {
+    //         return $this->checkOutServices->handleOrder($request->all());
+    //     }
+
+    //     if ($request->payment == "thanhtoannhanhang") {
+    //         $this->checkOutServices->handleOrder($request->all());
+    //         Toastr::success(null, 'Mua hàng thành công');
+    //         return redirect()->route('home');
+    //     }
+    // }
+
     public function index()
     {
+        ## Lấy userId ra
         $userId = Auth::id();
+        $orderId = request()->get('orderId');
+        $resultCode = request()->get('resultCode');
 
-        $orderId = request()->orderId ?: null;
-        $resultCode = request()->resultCode ?: null;
+        [$cart, $total] = $this->getCartData($userId);
 
-        $cart = [];
-        $total = 0;
-
-        if ($userId) {
-            [$cart, $total] = $this->cartServices->getCartWithTotal($userId);
-        } else {
-            if (session()->has('cart') && session()->get('total')) {
-                $cart = session('cart');
-                $total = session('total');
-            }
-        }
-
+        // Nếu data cart rỗng thì redirect
         if (empty($cart)) {
             return redirect()->route('shop.index');
         }
 
-        if (request()->has('orderId') && request()->has('resultCode') && $resultCode == 0) {
-            return $this->checkOutServices->isMomo();
+        // Nếu order thành công dựa vào orderId và resultCode == 0
+        if ($this->isValidMomoCallback($orderId, $resultCode)) {
+            return $this->checkOutServices->handleOrder($cart);
+            // dd(1);
         }
 
         return view(self::PATH_VIEW . 'check-out', compact('cart', 'total'));
@@ -67,5 +105,26 @@ class CheckOutController extends Controller
             Toastr::success(null, 'Mua hàng thành công');
             return redirect()->route('home');
         }
+    }
+
+    ##
+    private function getCartData($userId)
+    {
+        // Nếu có user thì lấy giỏ hàng trong db
+        if ($userId) {
+            return $this->cartServices->getCartWithTotal($userId);
+        }
+        // K có user thì lấy trong cart
+        if (session()->has('cart') && session()->has('total')) {
+            return [session('cart'), session('total')];
+        }
+
+        // Mặc định trả về mảng rỗng và 0
+        return [[], 0];
+    }
+
+    private function isValidMomoCallback($orderId, $resultCode)
+    {
+        return request()->has('orderId') && request()->has('resultCode') && $resultCode == 0;
     }
 }
--- app/Services/CheckOutServices.php
+++ app/Services/CheckOutServices.php
@@ -23,238 +23,291 @@ class CheckOutServices
         $this->momoService = $momoService;
     }
 
+    // public function handleOrder($data)
+    // {
+    //     $user = Auth::user();
+
+    //     try {
+
+
+    //         if ($user) {
+    //             $data['user_id'] = Auth::id();
+    //             $data['status_order'] ??= 'pending';
+    //             $data['status_payment'] ??= 'unpaid';
+
+
+    //             if ($data['payment'] === 'momo') {
+    //                 $data['status_payment'] = 'paid';
+    //                 session(['data-checkout' => $data]);
+    //                 // return $this->handleMomo($data['total_price']);
+    //                 return $this->momoService->redirectMomo($data['total_price']); // update new use service
+    //             }
+
+
+    //             return $this->createOrderAndOrderItem($data);
+    //         } else {
+    //             // Transaction
+    //             /**
+    //              * Thêm người dùng khi nhập form
+    //              * Lấy id của người dùng và truyền id để insert vào bảng order
+    //              * Lưu bảng order item
+    //              */
+
+    //             if ($data['payment'] == 'momo') {
+    //                 $data['status_payment'] = 'paid';
+    //                 session(['data-checkout' => $data]);
+    //                 return $this->momoService->redirectMomo($data['total_price']);
+    //             }
+
+    //             // dd($data);
+
+    //             DB::transaction(function () use ($data) {
+    //                 $user = User::create([
+    //                     'name' => $data['user_name'],
+    //                     'email' => $data['user_email'],
+    //                     'phone' => $data['user_phone'],
+    //                     'is_active' => 0,
+    //                     'role' => 'member'
+    //                 ]);
+
+    //                 $order = Order::create([
+    //                     'user_id' => $user->id,
+    //                     'user_name' => $user->name,
+    //                     'user_email' => $user->email,
+    //                     'user_phone' => $user->phone,
+    //                     'user_address' => $data['user_address'],
+    //                     'status_order' => 'pending',
+    //                     'status_payment' => 'unpaid',
+    //                     'total_price' => $data['total_price']
+    //                 ]);
+
+    //                 foreach ($data['product'] as $key => $product) {
+    //                     OrderItem::create([
+    //                         'order_id' => $order->id,
+    //                         'product_variant_id' => $key,
+    //                         'quantity' => $product['quantity'],
+    //                         'product_name' => $product['product_name'],
+    //                         'product_sku' => $product['product_sku'],
+    //                         'product_img_thumbnail' => $product['product_img_thumbnail'],
+    //                         'product_price_regular' => $product['product_price_regular'],
+    //                         'product_price_sale' => $product['product_price_sale'],
+    //                         'variant_size_name' => $product['variant_size_name'],
+    //                         'variant_color_name' => $product['variant_color_name'],
+    //                     ]);
+    //                 }
+    //             });
+
+    //             session()->forget('cart');
+    //             session()->forget('total');
+
+    //             return true;
+    //         }
+    //     } catch (\Throwable $th) {
+    //         Log::error($th->getMessage());
+    //         return false;
+    //     }
+    // }
+
+    // public function isMomo()
+    // {
+    //     // xử lý logic sau khi thanh toán thành công
+    //     $data = session('data-checkout');
+
+    //     $user = Auth::user();
+
+    //     if ($user) {
+    //         if (!empty($data)) {
+    //             $this->createOrderAndOrderItem($data);
+    //         }
+    //     } else {
+    //         DB::transaction(function () use ($data) {
+    //             $user = User::create([
+    //                 'name' => $data['user_name'],
+    //                 'email' => $data['user_email'],
+    //                 'phone' => $data['user_phone'],
+    //                 'is_active' => 0,
+    //                 'role' => 'member'
+    //             ]);
+
+    //             $order = Order::create([
+    //                 'user_id' => $user->id,
+    //                 'user_name' => $user->name,
+    //                 'user_email' => $user->email,
+    //                 'user_phone' => $user->phone,
+    //                 'user_address' => $data['user_address'],
+    //                 'status_order' => 'pending',
+    //                 'status_payment' => 'unpaid',
+    //                 'total_price' => $data['total_price']
+    //             ]);
+
+    //             foreach ($data['product'] as $key => $product) {
+    //                 OrderItem::create([
+    //                     'order_id' => $order->id,
+    //                     'product_variant_id' => $key,
+    //                     'quantity' => $product['quantity'],
+    //                     'product_name' => $product['product_name'],
+    //                     'product_sku' => $product['product_sku'],
+    //                     'product_img_thumbnail' => $product['product_img_thumbnail'],
+    //                     'product_price_regular' => $product['product_price_regular'],
+    //                     'product_price_sale' => $product['product_price_sale'],
+    //                     'variant_size_name' => $product['variant_size_name'],
+    //                     'variant_color_name' => $product['variant_color_name'],
+    //                 ]);
+    //             }
+    //         });
+
+    //         session()->forget('cart');
+    //         session()->forget('total');
+    //     }
+
+    //     Toastr::success(null, 'Thanh toán thành công');
+    //     return redirect()->route('shop.index');
+    // }
+
+    // protected function createOrderAndOrderItem($data)
+    // {
+    //     DB::transaction(function () use ($data) {
+    //         $order = $this->orderRepository->create($data);
+
+    //         foreach ($data['product'] as $value) {
+    //             $order->orderItems()->create([
+    //                 'product_variant_id' => $value['product_variant_id'],
+    //                 'quantity' => $value['quantity'],
+    //                 'product_name' => $value['product_name'],
+    //                 'product_sku' => $value['product_sku'],
+    //                 'product_img_thumbnail' => $value['product_img_thumbnail'],
+    //                 'product_price_regular' => $value['product_price_regular'],
+    //                 'product_price_sale' => $value['product_price_sale'],
+    //                 'variant_size_name' => $value['variant_size_name'],
+    //                 'variant_color_name' => $value['variant_color_name'],
+    //             ]);
+    //         }
+    //     }, 5);
+
+    //     $cart = Cart::where('user_id', Auth::id())->first();
+
+    //     if ($cart) {
+    //         $cart->cartItems()->delete();
+    //         $cart->delete();
+    //     }
+    // }
+
+
     public function handleOrder($data)
     {
         $user = Auth::user();
 
         try {
-
-
             if ($user) {
-                $data['user_id'] = Auth::id();
-                $data['status_order'] ??= 'pending';
-                $data['status_payment'] ??= 'unpaid';
-
-
-                if ($data['payment'] === 'momo') {
-                    $data['status_payment'] = 'paid';
-                    session(['data-checkout' => $data]);
-                    // return $this->handleMomo($data['total_price']);
-                    return $this->momoService->redirectMomo($data['total_price']); // update new use service
-                }
-
-
-                return $this->createOrderAndOrderItem($data);
-            } else {
-                // Transaction
-                /**
-                 * Thêm người dùng khi nhập form
-                 * Lấy id của người dùng và truyền id để insert vào bảng order
-                 * Lưu bảng order item
-                 */
-
-                if ($data['payment'] == 'momo') {
-                    $data['status_payment'] = 'paid';
-                    session(['data-checkout' => $data]);
-                    return $this->momoService->redirectMomo($data['total_price']);
-                }
-
-                // dd($data);
-
-                DB::transaction(function () use ($data) {
-                    $user = User::create([
-                        'name' => $data['user_name'],
-                        'email' => $data['user_email'],
-                        'phone' => $data['user_phone'],
-                        'is_active' => 0,
-                        'role' => 'member'
-                    ]);
-
-                    $order = Order::create([
-                        'user_id' => $user->id,
-                        'user_name' => $user->name,
-                        'user_email' => $user->email,
-                        'user_phone' => $user->phone,
-                        'user_address' => $data['user_address'],
-                        'status_order' => 'pending',
-                        'status_payment' => 'unpaid',
-                        'total_price' => $data['total_price']
-                    ]);
-
-                    foreach ($data['product'] as $key => $product) {
-                        OrderItem::create([
-                            'order_id' => $order->id,
-                            'product_variant_id' => $key,
-                            'quantity' => $product['quantity'],
-                            'product_name' => $product['product_name'],
-                            'product_sku' => $product['product_sku'],
-                            'product_img_thumbnail' => $product['product_img_thumbnail'],
-                            'product_price_regular' => $product['product_price_regular'],
-                            'product_price_sale' => $product['product_price_sale'],
-                            'variant_size_name' => $product['variant_size_name'],
-                            'variant_color_name' => $product['variant_color_name'],
-                        ]);
-                    }
-                });
-
-                session()->forget('cart');
-                session()->forget('total');
-
-                return true;
+                return $this->processAuthenticatedOrder($user, $data);
             }
+
+            return $this->processGuestOrder($data);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
             return false;
         }
     }
 
-    public function isMomo()
+    protected function processAuthenticatedOrder($user, array $data)
     {
-        // xử lý logic sau khi thanh toán thành công
-        $data = session('data-checkout');
+        $data['user_id'] = $user->id;
+        $data['status_order'] ??= 'pending';
+        $data['status_payment'] ??= 'unpaid';
 
-        $user = Auth::user();
-
-        if ($user) {
-            if (!empty($data)) {
-                $this->createOrderAndOrderItem($data);
-            }
-        } else {
-            DB::transaction(function () use ($data) {
-                $user = User::create([
-                    'name' => $data['user_name'],
-                    'email' => $data['user_email'],
-                    'phone' => $data['user_phone'],
-                    'is_active' => 0,
-                    'role' => 'member'
-                ]);
-
-                $order = Order::create([
-                    'user_id' => $user->id,
-                    'user_name' => $user->name,
-                    'user_email' => $user->email,
-                    'user_phone' => $user->phone,
-                    'user_address' => $data['user_address'],
-                    'status_order' => 'pending',
-                    'status_payment' => 'unpaid',
-                    'total_price' => $data['total_price']
-                ]);
-
-                foreach ($data['product'] as $key => $product) {
-                    OrderItem::create([
-                        'order_id' => $order->id,
-                        'product_variant_id' => $key,
-                        'quantity' => $product['quantity'],
-                        'product_name' => $product['product_name'],
-                        'product_sku' => $product['product_sku'],
-                        'product_img_thumbnail' => $product['product_img_thumbnail'],
-                        'product_price_regular' => $product['product_price_regular'],
-                        'product_price_sale' => $product['product_price_sale'],
-                        'variant_size_name' => $product['variant_size_name'],
-                        'variant_color_name' => $product['variant_color_name'],
-                    ]);
-                }
-            });
-
-            session()->forget('cart');
-            session()->forget('total');
+        ## Option thanh toán momo
+        if ($this->isMomoPayment($data)) {
+            return $this->redirectToMomo($data);
         }
 
-        Toastr::success(null, 'Thanh toán thành công');
-        return redirect()->route('shop.index');
+        // Option: thanh toán nhận hàng thì tạo đơn hàng mới
+        return $this->createOrderAndOrderItems($data);
     }
 
+    ##
 
-    public function execPostRequest($url, $data)
+    protected function processGuestOrder(array $data)
     {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data)
-            )
-        );
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        //execute post
-        $result = curl_exec($ch);
-        //close connection
-        curl_close($ch);
-        return $result;
+        // Option: thanh toán momo
+        if ($this->isMomoPayment($data)) {
+            return $this->redirectToMomo($data);
+        }
+        ;
+
+        // Optio: thanh toán nhận hàng
+        DB::transaction(function () use ($data) {
+            $user = $this->createGuestUser($data);
+            $data['user_id'] = $user->id;
+            $this->createOrderAndOrderItems($data);
+        });
+
+        $this->clearSessionCart();
+        return true;
     }
 
-    public function handleMomo($amount = 0)
+    ##
+    protected function redirectToMomo(array $data)
     {
-        $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
-
-        $partnerCode = 'MOMOBKUN20180529';
-        $accessKey = 'klm05TvNBzhg7h7j';
-        $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
-        $orderInfo = "Thanh toán qua MoMo";
-        // $amount = "10000";
-        $orderId = time() . "";
-        $redirectUrl = "https://laravel-intern.test/check-out";
-        $ipnUrl = "https://laravel-intern.test/check-out";
-        $extraData = "";
-
-        $requestId = time() . "";
-        $requestType = "payWithATM";
-        // $extraData = ($_POST["extraData"] ? $_POST["extraData"] : "");
-
-        //before sign HMAC SHA256 signature
-        $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
-        $signature = hash_hmac("sha256", $rawHash, $secretKey);
-        $data = array(
-            'partnerCode' => $partnerCode,
-            'partnerName' => "Test",
-            "storeId" => "MomoTestStore",
-            'requestId' => $requestId,
-            'amount' => $amount,
-            'orderId' => $orderId,
-            'orderInfo' => $orderInfo,
-            'redirectUrl' => $redirectUrl,
-            'ipnUrl' => $ipnUrl,
-            'lang' => 'vi',
-            'extraData' => $extraData,
-            'requestType' => $requestType,
-            'signature' => $signature
-        );
-        $result = $this->execPostRequest($endpoint, json_encode($data));
-        $jsonResult = json_decode($result, true);  // decode json
-
-        return redirect()->to($jsonResult['payUrl']);
+        session(['data-checkout' => $data]);
+        return $this->momoService->redirectMomo($data['total_price']);
     }
 
-    protected function createOrderAndOrderItem($data)
+    ##
+    protected function createGuestUser(array $data)
+    {
+        return User::create([
+            'name' => $data['user_name'],
+            'email' => $data['user_email'],
+            'phone' => $data['user_phone'],
+            'is_active' => 0,
+            'role' => 'member'
+        ]);
+    }
+
+    ##
+    protected function createOrderAndOrderItems(array $data)
     {
         DB::transaction(function () use ($data) {
+
             $order = $this->orderRepository->create($data);
 
-            foreach ($data['product'] as $value) {
+            foreach ($data as $key => $product) {
                 $order->orderItems()->create([
-                    'product_variant_id' => $value['product_variant_id'],
-                    'quantity' => $value['quantity'],
-                    'product_name' => $value['product_name'],
-                    'product_sku' => $value['product_sku'],
-                    'product_img_thumbnail' => $value['product_img_thumbnail'],
-                    'product_price_regular' => $value['product_price_regular'],
-                    'product_price_sale' => $value['product_price_sale'],
-                    'variant_size_name' => $value['variant_size_name'],
-                    'variant_color_name' => $value['variant_color_name'],
+                    'product_variant_id' => $product['product_variant_id'],
+                    'quantity' => $product['quantity'],
+                    'product_name' => $product['product_name'],
+                    'product_sku' => $product['product_sku'],
+                    'product_img_thumbnail' => $product['product_img_thumbnail'],
+                    'product_price_regular' => $product['product_price_regular'],
+                    'product_price_sale' => $product['product_price_sale'],
+                    'variant_size_name' => $product['variant_size_name'],
+                    'variant_color_name' => $product['variant_color_name'],
                 ]);
             }
-        }, 5);
+        });
 
-        $cart = Cart::where('user_id', Auth::id())->first();
+        // Sau khi tạo xong thì cart và cart_items
+        $this->clearUserCart();
+    }
 
-        if ($cart) {
+    ## clear cart
+    protected function clearUserCart()
+    {
+        if ($cart = Cart::where('user_id', Auth::id())->first()) {
             $cart->cartItems()->delete();
             $cart->delete();
         }
+    }
+
+    ## clear session
+    protected function clearSessionCart()
+    {
+        session()->forget(['cart', 'total']);
+    }
+
+    ## check isset payment is momo
+    protected function isMomoPayment($data)
+    {
+        return isset($data['payment']) && $data['payment'] === 'momo';
     }
 }
